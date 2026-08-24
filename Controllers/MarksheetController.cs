775422095790eeeefffc062e33015bdd3135d6d9
suresh <suresh@dcstechnosis.com>
2026-08-24 14:00:36 +0530

    Updated Code

--- Controllers/MarksheetController.cs
+++ Controllers/MarksheetController.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MarksheetApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace NIAUNIVERSITYPANEL.Controllers
 {
+   
     public class MarksheetController : Controller
     {
+      
+        [HttpGet]
         public IActionResult MS()
         {
             return View();
